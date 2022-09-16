50c2e6d6c38b7dcb68ce96e3556de40fa473a447
Ahasan Habib Sany <sany.baust@gmail.com>
2022-09-16 19:55:31 +0600

    serch done

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            Contact
-Contact
+      <section className="inner-hero bg_img overlay--one" style={{backgroundImage: 'url("https://script.viserlab.com/bloodlab/assets/images/frontend/breadcrumb/61023f89990b81627537289.jpg")', height:"300px"}}>
+            <div className="container">
+                <div className="row">
+                <div className="col-md-12 mb-5 pb-5
+                ">
+                    <h2 className="fw-bolder fs-1 text-white text-center mt-5">Contact</h2>                   
+                </div>
+                <div className="col-md-12 mt-5 pt-5">
+                </div>
+            </div>
+            </div>
+            </section>
         </div>
     );
 };
