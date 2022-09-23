a81d0272b2a36f9367e43a119d433b1b202aef92
Ahasan Habib Sany <sany.baust@gmail.com>
2022-09-23 18:50:10 +0600

    reg login start

--- server/index.js
+++ server/index.js
@@ -23,7 +23,54 @@ async function run() {
         const donorsCollection = client.db('blood-donor').collection('donors');
    
 
- 
+        app.post = async (req, res, next) => {
+
+            const { name, email, username, password } = req.body;
+        
+            const user = await User.findOne({
+                $or: [{ email }, { username }]
+            });
+            if (user) {
+                if (user.username === username) {
+                    return next(new ErrorHandler("Username already exists", 401));
+                }
+                return next(new ErrorHandler("Email already exists", 401));
+            }
+        
+            const newUser = await User.create({
+                name,
+                email,
+                username,
+                password,
+                avatar: req.file.location
+            })
+        
+            send(newUser, 201, res);
+        };
+        
+        // Login User
+        app.post = async (req, res, next) => {
+        
+            const { userId, password } = req.body;
+        
+            const user = await User.findOne({
+                $or: [{ email: userId }, { username: userId }]
+            }).select("+password");
+        
+            if (!user) {
+                return next(new ErrorHandler("User doesn't exist", 401));
+            }
+        
+            const isPasswordMatched = await user.comparePassword(password);
+        
+            if (!isPasswordMatched) {
+                return next(new ErrorHandler("Password doesn't match", 401));
+            }
+        
+            res.send(user, 201, res);
+        };
+        
+           
 
 
         app.get('/admin/:email', async (req, res) => {
